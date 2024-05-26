6c790c6afb1255a3893fb7e81c322ac458d4efeb
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-05-26 15:01:44 -0300

    feat: definindo a classe calculadora e adicionando lógica de atualização dos dígitos na tela

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,29 @@ const previousOperationText = document.querySelector('#previous-operation')
 const currentOperationText = document.querySelector('#current-operation')
 const buttons = document.querySelectorAll('#buttons-container button')
 
+//Defino uma classe para a calculadora
+class Calculator{
+    constructor(previousOperationText, currentOperationText){
+        //valores impressos na tela 
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        //valor que o usuário está digitando no momento
+        this.currentOperation = ''
+    }
+    //Adicionando digito na tela
+    addDigit(digit){
+        this.currentOperation = digit
+        this.updateScren()
+    }
+
+    //Atualiza os valores na tela ou seja mostra os valores dos botões que o usuário clicoi
+    updateScren(){
+        this.currentOperationText.innerText += this.currentOperation
+    }
+}
+
+const calc = new Calculator(previousOperationText,currentOperationText)
+
 //iterando  todos os botões para adicionar os listeners
 buttons.forEach((btn) =>{
     btn.addEventListener('click', (e) =>{
@@ -10,7 +33,7 @@ buttons.forEach((btn) =>{
         const value = e.target.innerText
         //verificando se o valor é um número ou uma operação
         if(+value >= 0 || value === '.'){
-            console.log(value)
+            calc.addDigit(value)
         }else{
             console.log('Op: '+ value)
         }
